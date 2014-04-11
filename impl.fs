049c8de7d5aa5126245b72a96eb4b9172829a00b
Mihai Veres <mihai.veres@ullink.com>
2014-04-11 18:23:38 +0300

    Initial commit
    Change-Id: I05f42d14b8b98ebb2e9015dee1c7ae5104628fcc

---
+++ impl.fs
@@ -0,0 +1,19 @@
+﻿module impl
+
+
+let makeGuess (guessHistory:list<string*string>) =
+    
+    let lastGuessEntry = guessHistory.[(List.length guessHistory) - 1]
+    if (snd lastGuessEntry) = "++++"
+    then ""
+    else 
+        let chr = (fst lastGuessEntry).[3]
+        let newChr = chr + (char)1
+        if newChr > 'F'
+        then ""
+        else "AAA" + newChr.ToString()
+        
+            
+
+        
+
---
+++ tests.fs
@@ -0,0 +1,35 @@
+﻿module tests
+
+open FsUnit
+open NUnit.Framework
+open impl
+
+[<Test>]
+let ``AAAA,++++ -> no new guess`` () =
+    makeGuess 
+        ["AAAA","++++"]
+    |> should equal ""
+
+[<Test>]
+let ``AAAA,+++ -> new guess is AAAB`` () =
+    makeGuess 
+        ["AAAA","+++"]
+    |> should equal "AAAB"
+
+[<Test>]
+let ``["AAAA","+++";"AAAB","+++"] -> new guess is AAAC`` () =
+    makeGuess 
+        ["AAAA","+++";
+         "AAAB","+++"]
+    |> should equal "AAAC"
+
+[<Test>]
+let ``["AAAA","+++"..."AAAF","+++"] -> no new guess`` () =
+    makeGuess 
+        ["AAAA","+++";
+         "AAAB","+++";
+         "AAAC","+++";
+         "AAAD","+++";
+         "AAAE","+++";
+         "AAAF","+++";]
+    |> should equal ""
